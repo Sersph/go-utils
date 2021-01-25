8661caf51accd619f83fb25c17b8f10cd04d06bd
winily <2675835744@qq.com>
2021-01-25 11:19:30 +0800

    feat: 字符串校验

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/winily/go-utils
+
+go 1.15
---
+++ string/verify.go
@@ -0,0 +1,14 @@
+package string
+
+import "strings"
+
+func IsEmpty(str string, ignoreSpaces bool) bool {
+	if ignoreSpaces {
+		str = strings.TrimSpace(str)
+	}
+	return str == "" || len(str) == 0
+}
+
+func IsNotEmpty(str string, ignoreSpaces bool) bool {
+	return !IsEmpty(str, ignoreSpaces)
+}
