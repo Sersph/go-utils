4f69c31ad5a1b362884886278c9102a63fa5efc8
kay <sersph@qq.com>
2021-01-25 22:22:42 +0800

    添加 sony的雪花算法生成ID

---
+++ generator/id_utils.go
@@ -0,0 +1,27 @@
+package generator
+
+import (
+	"github.com/sony/sonyflake"
+	"strconv"
+)
+
+func AmazonEC2MachineID() (uint16, error) {
+	var c uint16
+	c = 65
+	return c, nil
+}
+
+//采用sony的雪花算法生成主键id
+func GetSonyId() string {
+	var st sonyflake.Settings
+	st.MachineID = AmazonEC2MachineID
+	sf := sonyflake.NewSonyflake(st)
+	if sf == nil {
+		panic("sony flake not created")
+	}
+	id, err := sf.NextID()
+	if err != nil {
+		return ""
+	}
+	return strconv.FormatUint(id, 10)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/winily/go-utils
 
 go 1.15
+
+require github.com/sony/sonyflake v1.0.0
