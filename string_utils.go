f5fa55f83e53f1ceeb126de40cf76c9f8569e7da
winily <2675835744@qq.com>
2021-01-25 15:38:55 +0800

    feat: add is Blank func

--- string_utils.go
+++ string_utils.go
@@ -5,6 +5,12 @@ import (
 	"strings"
 )
 
+// ignoreSpaces 是否去掉前后空格再判断
+// isBlank("")         = true
+// isBlank(" ")        = false
+// isBlank("bob")      = false
+// isBlank("  bob  ")  = false
+// isBlank("\r\n\t\f") = false
 func IsEmpty(str string, ignoreSpaces bool) bool {
 	if ignoreSpaces {
 		str = strings.TrimSpace(str)
@@ -16,6 +22,11 @@ func IsNotEmpty(str string, ignoreSpaces bool) bool {
 	return !IsEmpty(str, ignoreSpaces)
 }
 
+// isBlank("")         = true
+// isBlank(" ")        = true
+// isBlank("bob")      = false
+// isBlank("  bob  ")  = false
+// isBlank("\r\n\t\f") = true
 func IsBlank(str string) bool {
 	result, _ := regexp.MatchString("^\\s*$", str)
 	return result
