52937e9d85695067b253556f490e6a9860d15011
winily <2675835744@qq.com>
2021-01-30 21:58:03 +0800

    style: 修改代码格式

--- conversion/string_utils.go
+++ conversion/string_utils.go
@@ -11,58 +11,55 @@ import (
 	非数值或字符类型的变量将会被转换成JSON格式字符串
 */
 func ToString(value interface{}) string {
-	var key string
 	if value == nil {
-		return key
+		return ""
 	}
 
 	switch value.(type) {
 	case float64:
 		ft := value.(float64)
-		key = strconv.FormatFloat(ft, 'f', -1, 64)
+		return strconv.FormatFloat(ft, 'f', -1, 64)
 	case float32:
 		ft := value.(float32)
-		key = strconv.FormatFloat(float64(ft), 'f', -1, 64)
+		return strconv.FormatFloat(float64(ft), 'f', -1, 64)
 	case int:
 		it := value.(int)
-		key = strconv.Itoa(it)
+		return strconv.Itoa(it)
 	case uint:
 		it := value.(uint)
-		key = strconv.Itoa(int(it))
+		return strconv.Itoa(int(it))
 	case int8:
 		it := value.(int8)
-		key = strconv.Itoa(int(it))
+		return strconv.Itoa(int(it))
 	case uint8:
 		it := value.(uint8)
-		key = strconv.Itoa(int(it))
+		return strconv.Itoa(int(it))
 	case int16:
 		it := value.(int16)
-		key = strconv.Itoa(int(it))
+		return strconv.Itoa(int(it))
 	case uint16:
 		it := value.(uint16)
-		key = strconv.Itoa(int(it))
+		return strconv.Itoa(int(it))
 	case int32:
 		it := value.(int32)
-		key = strconv.Itoa(int(it))
+		return strconv.Itoa(int(it))
 	case uint32:
 		it := value.(uint32)
-		key = strconv.Itoa(int(it))
+		return strconv.Itoa(int(it))
 	case int64:
 		it := value.(int64)
-		key = strconv.FormatInt(it, 10)
+		return strconv.FormatInt(it, 10)
 	case uint64:
 		it := value.(uint64)
-		key = strconv.FormatUint(it, 10)
+		return strconv.FormatUint(it, 10)
 	case string:
-		key = value.(string)
+		return value.(string)
 	case []byte:
-		key = string(value.([]byte))
+		return string(value.([]byte))
 	default:
 		newValue, _ := json.Marshal(value)
-		key = string(newValue)
+		return string(newValue)
 	}
-
-	return key
 }
 
 //将任意数组转换成 string 以指定字符进行分割
