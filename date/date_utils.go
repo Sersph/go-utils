f0b5ff0e2ee2d7a8f9cf8f2baa05c94210dfdb61
kay <sersph@qq.com>
2021-02-03 12:10:25 +0800

    添加 string 转 time

--- date/date_utils.go
+++ date/date_utils.go
@@ -1,7 +1,7 @@
 package date
 
-
 import (
+	"log"
 	"strconv"
 	"time"
 )
@@ -14,4 +14,23 @@ func GetYTD() string {
 
 	k := year + months + days
 	return k
+}
+
+/*
+	获取时间
+	参数：years 年、 months 月、 days 天
+	列： 0为当时， 整数表示往后， 负数表示往前
+	返回：2021-02-03 00:00:00
+*/
+func GetTime(years, months, days int) string {
+	return time.Now().AddDate(years, months, days).Format("2006-01-02 00:00:00")
+}
+
+//string 2006-10-02 10:00:00 转time
+func TimeParseString(data string) *time.Time {
+	times, err := time.Parse("2006-01-02 15:04:05", data)
+	if err != nil {
+		log.Fatalln("time parse err:", err)
+	}
+	return &times
 }
