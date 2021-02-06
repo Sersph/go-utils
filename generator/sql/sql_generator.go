ef1d3996b1daf16af9b33cd81ed61e9b63f5e809
lijiabao <2675835744@qq.com>
2021-02-06 16:31:38 +0800

    feat: ToSQL 功能

--- generator/sql/sql_generator.go
+++ generator/sql/sql_generator.go
@@ -1,6 +1,9 @@
 package sql
 
-import "github.com/winily/go-utils/conversion"
+import (
+	"github.com/winily/go-utils/conversion"
+	"reflect"
+)
 
 /**
 使用队列方式进行 sql 词组进行暂存
@@ -8,26 +11,37 @@ import "github.com/winily/go-utils/conversion"
 */
 type SelectBuilder struct {
 	// 查询字段队列
-	SelectPhrase *Queue
+	selectPhrase *Queue
 	// 查询表队列
-	FromPhrase *Queue
+	fromPhrase *Queue
 	// 查询条件队列
-	WherePhrase *Queue
+	wherePhrase *Queue
 }
 
 type ConditionFunc func(interface{}) bool
 
 func NewSelectBuilder() *SelectBuilder {
 	sb := SelectBuilder{}
-	sb.SelectPhrase = NewQueue()
-	sb.FromPhrase = NewQueue()
-	sb.WherePhrase = NewQueue()
+	sb.selectPhrase = NewQueue()
+	sb.fromPhrase = NewQueue()
+	sb.wherePhrase = NewQueue()
 	return &sb
+}
+
+func (sb *SelectBuilder) From(table interface{}) *SelectBuilder {
+	tableName := reflect.TypeOf(table).Name()
+	sb.fromPhrase.Enqueue(tableName)
+	return sb
+}
+
+func (sb *SelectBuilder) FromByName(table interface{}, tableName string) *SelectBuilder {
+	sb.fromPhrase.Enqueue(tableName)
+	return sb
 }
 
 func (sb *SelectBuilder) Eq(column string, val interface{}) *SelectBuilder {
 	where := ConstituteWherePhrase(column, EQ, conversion.ToString(val))
-	sb.WherePhrase.Enqueue(where)
+	sb.wherePhrase.Enqueue(where)
 	return sb
 }
 
@@ -50,4 +64,49 @@ func (sb *SelectBuilder) IsAllEq(condition ConditionFunc, params map[string]inte
 		sb.IsEq(condition, column, val)
 	}
 	return sb
+}
+
+func (sb *SelectBuilder) ToSQl() string {
+	if sb.fromPhrase.Size() == 0 {
+		panic("查询表不能为空")
+	}
+	return sb.ToSelect() + sb.ToFrom() + sb.ToWhere()
+}
+
+func (sb *SelectBuilder) ToSelect() string {
+	if sb.selectPhrase.Size() == 0 {
+		return SELECT + " * "
+	}
+
+	selectStr := SELECT
+	for sb.selectPhrase.Size() > 0 {
+		selectStr += " " + sb.selectPhrase.Dequeue() + " "
+	}
+	return selectStr
+}
+
+func (sb *SelectBuilder) ToWhere() string {
+	if sb.wherePhrase.Size() == 0 {
+		return ""
+	}
+
+	where := WHERE
+	for sb.wherePhrase.Size() > 0 {
+		where += " " + sb.wherePhrase.Dequeue() + " "
+		if sb.wherePhrase.Size() >= 1 {
+			where += AND
+		}
+	}
+	return where
+}
+
+func (sb *SelectBuilder) ToFrom() string {
+	from := FROM
+	for sb.fromPhrase.Size() > 0 {
+		from += " " + sb.fromPhrase.Dequeue() + " "
+		if sb.fromPhrase.Size() >= 1 {
+			from += ","
+		}
+	}
+	return from
 }
--- generator/sql/sql_utils_test.go
+++ generator/sql/sql_utils_test.go
@@ -5,18 +5,13 @@ import (
 	"testing"
 )
 
+type User struct {
+}
+
 func TestIdUtils(t *testing.T) {
-	var a float32 = 123
-	var b float64 = 123
-	var e int8 = 123
-	var f int16 = 123
-	var g uint8 = 123
-	var c complex128 = 123
-	fmt.Println(ConstituteWherePhrase("qq", EQ, "123"))
-	fmt.Println(ConstituteWherePhrase("qq", GE, a))
-	fmt.Println(ConstituteWherePhrase("qq", GT, b))
-	fmt.Println(ConstituteWherePhrase("qq", LT, e))
-	fmt.Println(ConstituteWherePhrase("qq", LE, f))
-	fmt.Println(ConstituteWherePhrase("qq", IS_NOT_NULL, g))
-	fmt.Println(ConstituteWherePhrase("qq", IS_NULL, c))
+	sqlBuilder := NewSelectBuilder()
+
+	sqlBuilder.From(User{}).Eq("name", "winily").Eq("age", 20).Eq("sex", 1)
+
+	fmt.Println(sqlBuilder.ToSQl())
 }
